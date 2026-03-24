7f9d324c2567ea2a937079d53844dcce700ac3bb
Vincent Kioko <vincentkioko51@gmail.com>
2026-03-25 01:00:52 +0300

    Exempted B2C callbacks from csrf middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            '/result',
+            '/timeout',
+        ]);
+
         $middleware->alias([
             'redirect.authenticated' => RedirectIfAuthenticated::class,
             'verified' => EnsureEmailVerified::class,
